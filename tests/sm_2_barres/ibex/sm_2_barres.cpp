ef68da9497b4a7de3fefa91453d8757013de7919
Alexandre Lefort <alexandre.lefort@ensta-bretagne.org>
2019-02-18 19:31:04 +0100

    Simplification ibex et ajout de docker

--- tests/sm_2_barres/ibex/sm_2_barres.cpp
+++ tests/sm_2_barres/ibex/sm_2_barres.cpp
@@ -35,16 +35,6 @@ void sm_2_barres() {
     x_ini[7] = Interval(kpt20 - eps_ctrl*fabs(kpt20), kpt20 + eps_ctrl*fabs(kpt20));
     x_ini[8] = Interval(kdt20 - eps_ctrl*fabs(kdt20), kdt20 + eps_ctrl*fabs(kdt20));
 
-    //x_ini[0] = Interval(-15.0, 15.0);
-    //x_ini[1] = Interval(-15.0, 15.0);
-    //x_ini[2] = Interval(-15.0, 15.0);
-    //x_ini[3] = Interval(-15.0, 15.0);
-    //x_ini[4] = Interval(  0.0,  0.1);
-    //x_ini[5] = Interval(-15.0, 15.0);
-    //x_ini[6] = Interval(-15.0, 15.0);
-    //x_ini[7] = Interval(-15.0, 15.0);
-    //x_ini[8] = Interval(-15.0, 15.0);
-
     double CzW0 = -2.7  ;
     double CmQ0 = -0.38 ;
     double eps  =  0.1  ;
@@ -62,9 +52,11 @@ void sm_2_barres() {
     p_ini[0] = Interval(CzW0 - eps*fabs(CzW0), CzW0 + eps*fabs(CzW0));
     p_ini[1] = Interval(CmQ0 - eps*fabs(CmQ0), CmQ0 + eps*fabs(CmQ0));
 
-    int num_thread = 4;
+    int num_thread = 2;
 
-    double x_prec(1e-8), y_prec(1e-8), stop_prec(0.1);
+    double x_prec(1e-10), y_prec(1e-10), stop_prec(0.1);
+
+    cout << "test1" << endl;
 
     std::vector<Function*>         coeffs   = std::vector<Function*>();   
     std::vector<Function*>         goals    = std::vector<Function*>();   
@@ -74,7 +66,9 @@ void sm_2_barres() {
     std::vector<NormalizedSystem*> fa_y_sys = std::vector<NormalizedSystem*>();
     std::vector<Ctc*>              x_ctc_id = std::vector<Ctc*>();          
     std::vector<Ctc*>              xy_ctc   = std::vector<Ctc*>();          
-    std::vector<Ctc*>              fa_y_ctc = std::vector<Ctc*>();          
+    std::vector<Ctc*>              fa_y_ctc = std::vector<Ctc*>();
+
+    cout << "test2" << endl;      
 
     for (int i = 0 ; i <num_thread ; i++) {
 
@@ -83,17 +77,16 @@ void sm_2_barres() {
         Function crit_hinf_zb1("../functions/Tzb1.txt" ) ;
         Function crit_hinf_zb2("../functions/Tzb2.txt" ) ; 
     
-        //coeffs.push_back(new Function("../functions/Tstab_coefs.txt"));
-//
-        //const ExprNode& stab1 = (*coeffs[i])(x,p)[0];
-        //const ExprNode& stab2 = (*coeffs[i])(x,p)[1];
-        //const ExprNode& stab3 = (*coeffs[i])(x,p)[2];
-        //const ExprNode& stab4 = (*coeffs[i])(x,p)[3];
-        //const ExprNode& stab5 = (*coeffs[i])(x,p)[4];
-        //const ExprNode& stab6 = (*coeffs[i])(x,p)[5];
+        coeffs.push_back(new Function("../functions/Tstab_coefs.txt"));
 
-        //stabs.push_back(new Function(x,p,ibex::max(ibex::max(ibex::max(ibex::max(-stab6,-stab4),-stab2),ibex::pow(stab1,2)*ibex::pow(stab6,2) + ibex::pow(stab2,2)*ibex::pow(stab5,2) + stab1*ibex::pow(stab4,2)*stab5 + stab2*ibex::pow(stab3,2)*stab6 - 2*stab1*stab2*stab5*stab6 - stab1*stab3*stab4*stab6 - stab2*stab3*stab4*stab5),stab1*stab4 - stab2*stab3)));
-        stabs.push_back(new Function(x,p,x[0] - 10000.0));
+        const ExprNode& stab1 = (*coeffs[i])(x,p)[0];
+        const ExprNode& stab2 = (*coeffs[i])(x,p)[1];
+        const ExprNode& stab3 = (*coeffs[i])(x,p)[2];
+        const ExprNode& stab4 = (*coeffs[i])(x,p)[3];
+        const ExprNode& stab5 = (*coeffs[i])(x,p)[4];
+        const ExprNode& stab6 = (*coeffs[i])(x,p)[5];
+
+        stabs.push_back(new Function(x,p,ibex::max(ibex::max(ibex::max(ibex::max(-stab6,-stab4),-stab2),ibex::pow(stab1,2)*ibex::pow(stab6,2) + ibex::pow(stab2,2)*ibex::pow(stab5,2) + stab1*ibex::pow(stab4,2)*stab5 + stab2*ibex::pow(stab3,2)*stab6 - 2*stab1*stab2*stab5*stab6 - stab1*stab3*stab4*stab6 - stab2*stab3*stab4*stab5),stab1*stab4 - stab2*stab3)));
         Function f_max1(x,y,crit_hinf_zz1(x,y));
         Function f_max2(x,y,crit_hinf_zz2(x,y));
         Function f_max3(x,y,crit_hinf_zb1(x,y));
@@ -127,7 +120,7 @@ void sm_2_barres() {
 
     OptimMinMax oo(sys_x, sys_xy, fa_y_sys, x_ctc_id, xy_ctc, fa_y_ctc, x_prec, y_prec,stop_prec,prec_fa_y, num_thread);
     
-    oo.list_elem_absolute_max = 2000;
+    oo.list_elem_absolute_max = 500;
     oo.list_rate = 0;
     oo.critpr = 0.4;
     oo.heap_prob = 0.5;
@@ -136,7 +129,7 @@ void sm_2_barres() {
     oo.visit_all = false;
     oo.nb_point = 1;
 
-    oo.list_elem_absolute_max_csp = 800;
+    oo.list_elem_absolute_max_csp = 500;
     oo.iter_csp = 10;
     oo.critpr_csp = 0.3;
     oo.list_rate_csp = 0;
@@ -145,7 +138,7 @@ void sm_2_barres() {
 
     oo.trace=1;
     oo.trace_freq = 1;
-    oo.timeout=3600*7;
+    oo.timeout=600;
 
     Optim::Status res = oo.optimize(x_ini);
 
